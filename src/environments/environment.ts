8dcb61079fa18956bf7cdeeb150093c056e995fa
Samer Kinaan <52113379+samerios@users.noreply.github.com>
2025-01-04 20:01:57 +0200

    Pre continues integration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     apiUrl: 'api/',
     hubUrl: 'hub/notifications',
-    stripePublicKey: 'pk_test_51QcrunA42VIrHN16ZWUnyDiPtHz3lSyssuCFelXdFarU0oUSgOi7gcdpIbnAeVH4pHOqa0af9ZXZ1c3fgwcS8VpU00Lqm1uioS'
+    stripePublicKey: 'pk_test_51QdXB4AVHkXmBB2ugeLmS7UFsoR8g9h1n4AzDFAGfpyAKp9W6NNvLrFT5k37Rsfzs7HsOIIeshCRvUKTtlLoEaLb00JsqJjrTy'
 };
